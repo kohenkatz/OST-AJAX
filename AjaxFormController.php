f9ea6dae635a7fe950ccb0149ddbc2c791c429ce
Moshe Katz <moshe@ymkatz.net>
2015-12-23 22:13:00 -0500

    add form topics, form prefix, captcha and debug output

--- AjaxFormController.php
+++ AjaxFormController.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once(__dir__.DIRECTORY_SEPARATOR.'config.php');
+require_once(__dir__.DIRECTORY_SEPARATOR.'Encryption.php');
 require_once(INCLUDE_DIR.'/class.forms.php');
 
 class AjaxFormController {
@@ -12,7 +13,9 @@ class AjaxFormController {
 	}
 
 	public function get_open() {
-		global $ost;
+		global $ost, $cfg;
+
+		define('MY_DEBUG', isset($_GET['debug']) && ($_GET['debug'] === '1'));
 
 		$topic_names = Topic::getPublicHelpTopics();
 		if (isset($_GET['topics'])) {
@@ -28,9 +31,59 @@ class AjaxFormController {
 			}, ARRAY_FILTER_USE_BOTH);
 		}
 
-		$topics = [];
+		$formPrefix = $this->config->get('ajax_submission_form_prefix');
 
-		$captcha = [];
+		$csrf_token = $ost->getCSRF()->getToken();
+
+		$topics = [];
+		foreach ($topic_names as $id => $name) {
+			if (($topic = Topic::lookup($id)) &&
+				($form = $topic->getForm()) &&
+				($fields = $form->getForm()->getFields())) {
+					$topics[$id] = [
+						'name' => $name,
+						'instructions' => $form->get('instructions'),
+						'fields' => [],
+					];
+					foreach ($fields as $field) {
+						$topics[$id]['fields'][$field->get('name')] = [
+							'name' => $formPrefix.$field->get('name'),
+							'type' => $field->get('type'),
+						];
+						if ($field->get('type') === 'choices') {
+							$topics[$id]['fields'][$field->get('name')]['choices'] = $field->getChoices();
+						}
+					}
+			}
+		}
+
+		$captcha = null;
+		$useCaptcha = $this->config->get('ajax_submission_captcha');
+		if ($useCaptcha === 1) { // built-in
+			$captcha = [
+				'type' => 'osTicket',
+				// FILL IN
+			];
+		} elseif ($useCaptcha === 2) { // recaptcha
+			$public = $this->config->get('ajax_recaptcha_site');
+			$private = $this->config->get('ajax_recaptcha_secret');
+			if ($public && $private) {
+				// Key creation based on https://github.com/google/recaptcha-java/blob/master/appengine/src/main/java/com/google/recaptcha/STokenUtils.java
+				$encrypter = new Encryption(substr(hash('sha1', $private, true), 0, 16));
+				$secure_token = json_encode([
+					// PHP unique ID prefixed with the csrf token
+					'session_id' => uniqid($csrf_token),
+					'ts_ms' => bcmul(microtime(true), 1000, 0),
+				]);
+				$captcha = [
+					'type' => 'reCaptcha',
+					'public_key' => $public,
+					'secure_token' => $encrypter->encrypt_aes_ecb_pkcs5($secure_token),
+				];
+			} else {
+				throw new Exception('reCaptcha key not provided!');
+			}
+		}
 
 		$return = [
 			'method' => 'POST',
@@ -39,50 +92,56 @@ class AjaxFormController {
 				'backend'       => ['hidden' => true, fields => ['csrf', 'action',],],
 				'topic'         => ['legend' => 'Help Topic', fields => ['topicId',],],
 				'user'          => ['legend' => 'Contact Information', fields => ['name', 'email', 'phone',],],
-				'topic_details' => ['dynamic' => true, 'id' => 'ost_ajaxform_topic_details'],
+				'topic_details' => ['dynamic' => true, 'id' => $formPrefix.'topic_details'],
 				'ticket'        => ['legend' => 'Ticket Details', fields => ['summary', 'details',],],
 			],
 			'form_fields' => [
 				'csrf' => [
 					'type' => 'hidden',
-					'value' => $ost->getCSRF()->getToken(),
-					'name' => $ost->getCSRF()->getTokenName(),
+					'value' => $csrf_token,
+					'name' => $formPrefix.$ost->getCSRF()->getTokenName(),
 				],
 				'action' => [
 					'type' => 'hidden',
 					'value' => 'open',
-					'name' => 'a',
+					'name' => $formPrefix.'a',
 				],
 				'topicId' => [
+					'label' => 'Select a Topic',
 					'required' => true,
-					'type' => 'select',
-					'name' => 'topicId',
-					'options' => $topic_names,
+					'type' => 'choices',
+					'name' => $formPrefix.'topicId',
+					'choices' => $topic_names,
 				],
 				'name' => [
+					'label' => 'Full Name',
 					'required' => true,
 					'type' => 'text',
-					'name' => 'name',
+					'name' => $formPrefix.'name',
 				],
 				'email' => [
+					'label' => 'Email Address',
 					'required' => true,
 					'type' => 'text',
-					'name' => 'email',
+					'name' => $formPrefix.'email',
 				],
 				'phone' => [
+					'label' => 'Phone Number',
 					'required' => true,
 					'type' => 'text',
-					'name' => 'phone',
+					'name' => $formPrefix.'phone',
 				],
 				'summary' => [
+					'label' => 'Issue Summary',
 					'required' => true,
 					'type' => 'text',
-					'name' => 'summary',
+					'name' => $formPrefix.'summary',
 				],
 				'details' => [
+					'label' => 'Issue Details',
 					'required' => true,
 					'type' => 'textarea',
-					'name' => 'details',
+					'name' => $formPrefix.'details',
 				],
 			],
 			'topics' => $topics,
@@ -91,7 +150,12 @@ class AjaxFormController {
 
 		header('Access-Control-Allow-Origin: '. $this->config->get('ajax_cors_header'));
 
-		return json_encode($return);
+		$json_flags = 0;
+		if (MY_DEBUG) {
+			$json_flags += JSON_PRETTY_PRINT;
+		}
+
+		return json_encode($return, $json_flags);
 	}
 
 	public function post_submit() {
